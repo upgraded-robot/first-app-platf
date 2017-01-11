cced9512d24e47a4fc9c93e09803ffe68fd177e2
upgraded-robot <mariadotdev.ln@gmail.com>
2017-01-11 15:07:05 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Mi primera aplicación</h1>
+        <p className="App-intro">
+          Esta es mi primera aplicación en React, está padrísimo!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
